b531679a616d14eebf6e665572093212ce33e5fd
Alberto Mengali <disberd@gmail.com>
2023-07-13 10:20:54 +0200

    deploy

--- docs/make.jl
+++ docs/make.jl
@@ -22,3 +22,17 @@ makedocs(;
         "Home" => "index.md",
     ],
 )
+
+# This controls whether or not deployment is attempted. It is based on the value
+# of the `SHOULD_DEPLOY` ENV variable, which defaults to the `CI` ENV variables or
+# false if not present.
+should_deploy = get(ENV,"SHOULD_DEPLOY", get(ENV, "CI", "") === "true")
+
+if should_deploy
+    @info "Deploying"
+
+deploydocs(
+    repo = "github.com/disberd/PlotlyDocumenter.jl.git",
+)
+
+end
